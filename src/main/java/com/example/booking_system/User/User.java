39f6d0cb3ffdea4ed0af0b55745416af68c2679c
Viktoria <vs118475@gmail.com>
2026-06-20 16:08:28 +0300

    Создала User

---
+++ src/main/java/com/example/booking_system/User/User.java
@@ -0,0 +1,13 @@
+package com.example.booking_system.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private Long id;
+}
