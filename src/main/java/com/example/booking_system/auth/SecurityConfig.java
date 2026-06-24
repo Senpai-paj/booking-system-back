64e593ae08e3e83a6936aaa25f21a65ea3ab241a
Senpai-paj <victor.pirojoc@gmail.com>
2026-06-24 11:14:38 +0200

    added cors to the back-end

--- src/main/java/com/example/booking_system/auth/SecurityConfig.java
+++ src/main/java/com/example/booking_system/auth/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -33,6 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable) // not needed for stateless token auth
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
